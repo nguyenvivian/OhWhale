43c4f9e9383d735be50821fc21b56bc01c202e4b
Kulraj Dhaliwal <kulrajd@uci.edu>
2020-02-01 23:25:52 -0800

    Sending sms

--- src/main/java/SmsApp.java
+++ src/main/java/SmsApp.java
@@ -6,21 +6,14 @@ import static spark.Spark.*;
 
 public class SmsApp {
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello Web");
-
+        get("/", (req, res) -> "Hello");
+    
         post("/sms", (req, res) -> {
+        	String body2 = req.queryParams("Body");
             res.type("application/xml");
-            Body body = new Body
-                    .Builder("The Robots are coming! Head for the hills!")
-                    .build();
-            Message sms = new Message
-                    .Builder()
-                    .body(body)
-                    .build();
-            MessagingResponse twiml = new MessagingResponse
-                    .Builder()
-                    .message(sms)
-                    .build();
+            Body body = new Body.Builder(body2).build();
+            Message sms = new Message.Builder().body(body).build();
+            MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
             return twiml.toXml();
         });
     }
