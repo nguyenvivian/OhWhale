96d58fc775460cb3ae3404488524e6fb1e3be68e
Alexis Vu <alexis.lauren.vu@gmail.com>
2020-02-01 20:50:10 -0800

    no message

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,9 +1,69 @@
 package src.main.java;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Demo {
 
 	public static void main(String[] args) {
+		int numEnemies, rowSize, colSize, maxEnemies;
+		Scanner consoleScanner = new Scanner(System.in);
 
+		System.out.println("Welcome to Oh Whale!");
+		rowSize = 7;
+		colSize = 7;
+		maxEnemies = (rowSize * colSize) - 1;
+		numEnemies = validateInput(consoleScanner, "How many oil spills have infected the ocean?", 1, maxEnemies,
+				"Please enter a number of spills > 0 and less than or equal to " + maxEnemies);
+
+		Board gameBoard = new Board(rowSize, colSize, numEnemies);
+		System.out.println("\n" + gameBoard + "\n");
+		do {
+			gameBoard.moveAllCharacters();
+			System.out.println(gameBoard);
+
+		} while (!gameBoard.isGameOver());
+
+		if (gameBoard.isPlayerAlive())
+			System.out.println("You win! Congratulations elminating all of your enemies.");
+		else
+			System.err.println("Player has died.");
+	}
+	
+	public static int validateInput(Scanner consoleScanner, String message, int min, int max, String errorMessage) {
+		int input = 0;
+		boolean inputError;
+		do {
+			try {
+				inputError = false;
+				System.out.print(message + " ");
+				input = consoleScanner.nextInt();
+				if (input < min || input > max)
+					inputError = true;
+			} catch (InputMismatchException e) {
+				inputError = true;
+				consoleScanner.next();
+			}
+			if (inputError)
+				System.err.println(errorMessage + "\n");
+
+		} while (inputError);
+		return input;
 	}
 
+	/**
+	 * Validates user input to prompt them with a message, read an int from the consoleScanner, validate that it is > min.
+	 * If the input is valid, it is returned, otherwise a user is prompted again in a loop until valid input
+	 * is entered.  Reuses existing code from <code>isValidInput</code> giving Integer.MAX_VALUE for the maximum value (e.g. no max).
+	 *
+	 * @param consoleScanner Used to read user input from the console.
+	 * @param message The message to display to the user.
+	 * @param min The minimum value allowed.
+	 * @param errorMessage The error message to display to the user if input is invalid.
+	 * @return The validated input from the user.
+	 */
+	public static int validateInputMin(Scanner consoleScanner, String message, int min, String errorMessage) {
+		return validateInput(consoleScanner, message, min, Integer.MAX_VALUE, errorMessage);
+	}
 }
+
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,59 @@
 package src.main.java;
+
+import java.util.Scanner;
+
 public class Player extends Character implements GameActions {
 	public Player(int row, int col) {
 		super(row, col);
 	}
 	
 	void move(Board board) {
-		// TODO: implement move logic in game
+		Scanner cs = new Scanner(System.in);
+		String choice;
+		
+		System.out.print("What action would you like the player to take?\n\n"
+				+ "U: Move Up\n" + "D: Move Down\n" + "L: Move Left\n"
+				+ "R: Move Right\n" + "W: Pellet\n" + "Move: ");
+		
+		choice = cs.nextLine();
+		choice = choice.toUpperCase();
+		System.out.println();
+		
+		if (board.isGameOver())
+			cs.close();
+		
+		switch (choice)
+		{
+			case "U":
+				if (board.isInBounds(mRow - 1, mCol))
+					--mRow;
+				else
+					System.out.println("Player currently cannot move UP and loses turn.\n");
+			break;
+			case "D":
+				if (board.isInBounds(mRow + 1, mCol))
+					++mRow;
+				else
+					System.out.println("Player currently cannot move DOWN and loses turn.\n");
+				break;
+			case "L":
+				if (board.isInBounds(mRow, mCol - 1))
+					--mCol; 
+				else
+					System.out.println("Player currently cannot move LEFT and loses turn.\n");
+				break;
+			case "R":
+				if (board.isInBounds(mRow, mCol + 1))
+					++mCol;
+				else
+					System.out.println("Player currently cannot move RIGHT and loses turn.\n");
+				break;
+			case "W":
+				board.addWeapon(mRow, mCol);
+				break;
+			default: System.out.println("Unrecognized action, please enter U, D, L, R, or W. Player loses turn.\n");
+		}		
+
 	}
 	
 	public String toString() {
