b28f79d399f0ad47bee67eb0d1940986840a0497
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2020-02-03 01:36:04 -0800

    Final submission

--- src/main/java/SmsSender.java
+++ src/main/java/SmsSender.java
@@ -5,16 +5,16 @@ import com.twilio.type.PhoneNumber;
 public class SmsSender {
     // Find your Account Sid and Auth Token at twilio.com/console
     public static final String ACCOUNT_SID =
-            "ACe2ace5ecbc47778a06fb34c8598b98c6";
+            "";
     public static final String AUTH_TOKEN =
-            "a2d150381dca1bb47e221fd0acdb58ba";
+            "";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message
-                .creator(new PhoneNumber("+17144735800"), // to
-                        new PhoneNumber("+18509192460"), // from
+                .creator(new PhoneNumber("+"), // to
+                        new PhoneNumber("+"), // from
                         "hello there")
                 .create();
 
